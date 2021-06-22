c9342ed811bfcf0794c9085505b643b3ae3a66bf
mhoffrog <>
2021-06-22 19:12:54 +0200

    Added sources for JDK8u144-b34

---
+++ openjdk8-jaxp-external/src-gen/jaxp/jdk8u/jaxp/org/apache/xpath/external/axes/HasPositionalPredChecker.java
@@ -0,0 +1,125 @@
+/*
+ * reserved comment block
+ * DO NOT REMOVE OR ALTER!
+ */
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * $Id: HasPositionalPredChecker.java,v 1.1.2.1 2005/08/01 01:30:24 jeffsuttor Exp $
+ */
+package jdk8u.jaxp.org.apache.xpath.external.axes;
+
+import jdk8u.jaxp.org.apache.xpath.external.Expression;
+import jdk8u.jaxp.org.apache.xpath.external.ExpressionOwner;
+import jdk8u.jaxp.org.apache.xpath.external.XPathVisitor;
+import jdk8u.jaxp.org.apache.xpath.external.functions.FuncLast;
+import jdk8u.jaxp.org.apache.xpath.external.functions.FuncPosition;
+import jdk8u.jaxp.org.apache.xpath.external.functions.Function;
+import jdk8u.jaxp.org.apache.xpath.external.objects.XNumber;
+import jdk8u.jaxp.org.apache.xpath.external.operations.Div;
+import jdk8u.jaxp.org.apache.xpath.external.operations.Minus;
+import jdk8u.jaxp.org.apache.xpath.external.operations.Mod;
+import jdk8u.jaxp.org.apache.xpath.external.operations.Mult;
+import jdk8u.jaxp.org.apache.xpath.external.operations.Plus;
+import jdk8u.jaxp.org.apache.xpath.external.operations.Quo;
+import jdk8u.jaxp.org.apache.xpath.external.operations.Variable;
+
+public class HasPositionalPredChecker extends XPathVisitor
+{
+        private boolean m_hasPositionalPred = false;
+        private int m_predDepth = 0;
+
+        /**
+         * Process the LocPathIterator to see if it contains variables
+         * or functions that may make it context dependent.
+         * @param path LocPathIterator that is assumed to be absolute, but needs checking.
+         * @return true if the path is confirmed to be absolute, false if it
+         * may contain context dependencies.
+         */
+        public static boolean check(LocPathIterator path)
+        {
+                HasPositionalPredChecker hppc = new HasPositionalPredChecker();
+                path.callVisitors(null, hppc);
+                return hppc.m_hasPositionalPred;
+        }
+
+        /**
+         * Visit a function.
+         * @param owner The owner of the expression, to which the expression can
+         *              be reset if rewriting takes place.
+         * @param func The function reference object.
+         * @return true if the sub expressions should be traversed.
+         */
+        public boolean visitFunction(ExpressionOwner owner, Function func)
+        {
+                if((func instanceof FuncPosition) ||
+                   (func instanceof FuncLast))
+                        m_hasPositionalPred = true;
+                return true;
+        }
+
+//      /**
+//       * Visit a variable reference.
+//       * @param owner The owner of the expression, to which the expression can
+//       *              be reset if rewriting takes place.
+//       * @param var The variable reference object.
+//       * @return true if the sub expressions should be traversed.
+//       */
+//      public boolean visitVariableRef(ExpressionOwner owner, Variable var)
+//      {
+//              m_hasPositionalPred = true;
+//              return true;
+//      }
+
+  /**
+   * Visit a predicate within a location path.  Note that there isn't a
+   * proper unique component for predicates, and that the expression will
+   * be called also for whatever type Expression is.
+   *
+   * @param owner The owner of the expression, to which the expression can
+   *              be reset if rewriting takes place.
+   * @param pred The predicate object.
+   * @return true if the sub expressions should be traversed.
+   */
+  public boolean visitPredicate(ExpressionOwner owner, Expression pred)
+  {
+    m_predDepth++;
+
+    if(m_predDepth == 1)
+    {
+      if((pred instanceof Variable) ||
+         (pred instanceof XNumber) ||
+         (pred instanceof Div) ||
+         (pred instanceof Plus) ||
+         (pred instanceof Minus) ||
+         (pred instanceof Mod) ||
+         (pred instanceof Quo) ||
+         (pred instanceof Mult) ||
+         (pred instanceof jdk8u.jaxp.org.apache.xpath.external.operations.Number) ||
+         (pred instanceof Function))
+          m_hasPositionalPred = true;
+      else
+        pred.callVisitors(owner, this);
+    }
+
+    m_predDepth--;
+
+    // Don't go have the caller go any further down the subtree.
+    return false;
+  }
+
+
+}
---
+++ openjdk8-jaxp-external/src-gen/jaxp/jdk8u/jaxp/org/apache/xpath/external/functions/FuncCurrent.java
@@ -0,0 +1,84 @@
+/*
+ * reserved comment block
+ * DO NOT REMOVE OR ALTER!
+ */
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * $Id: FuncCurrent.java,v 1.2.4.1 2005/09/14 19:53:44 jeffsuttor Exp $
+ */
+package jdk8u.jaxp.org.apache.xpath.external.functions;
+
+import jdk8u.jaxp.org.apache.xml.external.dtm.DTM;
+import jdk8u.jaxp.org.apache.xml.external.dtm.DTMIterator;
+import jdk8u.jaxp.org.apache.xpath.external.XPathContext;
+import jdk8u.jaxp.org.apache.xpath.external.axes.LocPathIterator;
+import jdk8u.jaxp.org.apache.xpath.external.axes.PredicatedNodeTest;
+import jdk8u.jaxp.org.apache.xpath.external.objects.XNodeSet;
+import jdk8u.jaxp.org.apache.xpath.external.objects.XObject;
+import jdk8u.jaxp.org.apache.xpath.external.axes.SubContextList;
+import jdk8u.jaxp.org.apache.xpath.external.patterns.StepPattern;
+import jdk8u.jaxp.org.apache.xalan.external.res.XSLMessages;
+import jdk8u.jaxp.org.apache.xalan.external.res.XSLTErrorResources;
+
+
+/**
+ * Execute the current() function.
+ * @xsl.usage advanced
+ */
+public class FuncCurrent extends Function
+{
+    static final long serialVersionUID = 5715316804877715008L;
+
+  /**
+   * Execute the function.  The function must return
+   * a valid object.
+   * @param xctxt The current execution context.
+   * @return A valid XObject.
+   *
+   * @throws javax.xml.transform.TransformerException
+   */
+  public XObject execute(XPathContext xctxt) throws javax.xml.transform.TransformerException
+  {
+
+    SubContextList subContextList = xctxt.getCurrentNodeList();
+    int currentNode = DTM.NULL;
+
+    if (null != subContextList) {
+        if (subContextList instanceof PredicatedNodeTest) {
+            LocPathIterator iter = ((PredicatedNodeTest)subContextList)
+                                                          .getLocPathIterator();
+            currentNode = iter.getCurrentContextNode();
+         } else if(subContextList instanceof StepPattern) {
+           throw new RuntimeException(XSLMessages.createMessage(
+              XSLTErrorResources.ER_PROCESSOR_ERROR,null));
+         }
+    } else {
+        // not predicate => ContextNode == CurrentNode
+        currentNode = xctxt.getContextNode();
+    }
+    return new XNodeSet(currentNode, xctxt.getDTMManager());
+  }
+
+  /**
+   * No arguments to process, so this does nothing.
+   */
+  public void fixupVariables(java.util.Vector vars, int globalsSize)
+  {
+    // no-op
+  }
+
+}
